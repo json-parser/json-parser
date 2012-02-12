c5c8d6dbc36cd68a0685970ddb9d2e8416cffb78
James McLaughlin <jamie@lacewing-project.org>
2012-02-12 23:21:40 +0000

    And another fix for hex_value

--- json.c
+++ json.c
@@ -44,10 +44,10 @@ typedef unsigned short json_uchar;
 
 static unsigned char hex_value (json_char c)
 {
-   if (c >= 'A' && c <= 'Z')
+   if (c >= 'A' && c <= 'F')
       return (c - 'A') + 10;
 
-   if (c >= 'a' && c <= 'z')
+   if (c >= 'a' && c <= 'f')
       return (c - 'a') + 10;
 
    if (c >= '0' && c <= '9')

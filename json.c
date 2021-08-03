d27c598e9ea501f7c73a9e8eec4a814bb58516c7
Per Olav Kroka <per.olav.kroka@speo.no>
2021-08-03 13:13:49 +0200

    Remove dependency to the math library.
    Introducing json_pow10(), a fast power of 10 calculator,
    which suits JSON perfectly. (Named json_pow10 to avoid
    conflict with gcc builtin functions.)

--- json.c
+++ json.c
@@ -40,7 +40,6 @@
 #include <string.h>
 #include <ctype.h>
 #include <limits.h>
-#include <math.h>
 
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
    /* C99 might give us uintptr_t and UINTPTR_MAX but they also might not be provided */
@@ -68,6 +67,31 @@
 typedef unsigned int json_uchar;
 
 const struct _json_value json_value_none;
+
+static double json_pow10 (int exp)
+{
+  double base = 10.0;
+  double result = 1.0;
+  int neg = exp < 0;
+
+  exp = !neg ? exp : -exp;
+  exp = exp <= 308 ? exp : 309; /* double max exp = 308, allows the "infinite" number */
+
+  while (1)
+  {
+    if (exp & 1)
+      result *= base;
+
+    exp >>= 1;
+
+    if (!exp)
+      break;
+
+    base *= base;
+  }
+
+  return !neg ? result : 1 / result;
+}
 
 static unsigned char hex_value (json_char c)
 {
@@ -846,7 +870,7 @@ json_value * json_parse_ex (json_settings * settings,
                         goto e_failed;
                      }
 
-                     top->u.dbl += num_fraction / pow (10.0, num_digits);
+                     top->u.dbl += num_fraction / json_pow10 (num_digits);
                   }
 
                   if (b == 'e' || b == 'E')
@@ -873,7 +897,7 @@ json_value * json_parse_ex (json_settings * settings,
                      goto e_failed;
                   }
 
-                  top->u.dbl *= pow (10.0, (flags & flag_num_e_negative ? - num_e : num_e));
+                  top->u.dbl *= json_pow10 ((flags & flag_num_e_negative ? - num_e : num_e));
                }
 
                if (flags & flag_num_negative)

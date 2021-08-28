1ab9a75e74770993ef7c118683964c3e3fef8d44
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2021-08-28 23:39:46 +0300

    Use size_t for memory size
    That's the standard over all the C standard library.

--- json.c
+++ json.c
@@ -78,10 +78,10 @@ static int would_overflow (json_int_t value, json_char b)
 
 typedef struct
 {
-   unsigned long used_memory;
+   size_t used_memory;
 
    unsigned int uint_max;
-   unsigned long ulong_max;
+   size_t size_max;
 
    json_settings settings;
    int first_pass;
@@ -101,9 +101,9 @@ static void default_free (void * ptr, void * user_data)
    free (ptr);
 }
 
-static void * json_alloc (json_state * state, unsigned long size, int zero)
+static void * json_alloc (json_state * state, size_t size, int zero)
 {
-   if ((state->ulong_max - state->used_memory) < size)
+   if ((state->size_max - state->used_memory) < size)
       return 0;
 
    if (state->settings.max_memory
@@ -120,7 +120,7 @@ static int new_value (json_state * state,
                       json_type type)
 {
    json_value * value;
-   int values_size;
+   size_t values_size;
 
    if (!state->first_pass)
    {
@@ -154,7 +154,7 @@ static int new_value (json_state * state,
             values_size = sizeof (*value->u.object.values) * value->u.object.length;
 
             if (! (value->u.object.values = (json_object_entry *) json_alloc
-                  (state, values_size + ((unsigned long) value->u.object.values), 0)) )
+                  (state, values_size + ((size_t) value->u.object.values), 0)) )
             {
                return 0;
             }
@@ -270,10 +270,10 @@ json_value * json_parse_ex (json_settings * settings,
       state.settings.mem_free = default_free;
 
    memset (&state.uint_max, 0xFF, sizeof (state.uint_max));
-   memset (&state.ulong_max, 0xFF, sizeof (state.ulong_max));
+   memset (&state.size_max, 0xFF, sizeof (state.size_max));
 
    state.uint_max -= 8; /* limit of how much can be added before next check */
-   state.ulong_max -= 8;
+   state.size_max -= 8;
 
    for (state.first_pass = 1; state.first_pass >= 0; -- state.first_pass)
    {

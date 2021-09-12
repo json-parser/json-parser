5b44508051d4d27da177650d9277b2ed3079fd45
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2021-09-12 14:38:48 +0200

    Fix flake8 alerts

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,3 @@
-
 # See bindings/python for the python jsonparser module
 
 import jsonparser
@@ -16,7 +15,7 @@ for i, test in enumerate(
     try:
         jsonparser.decode(test)
     except jsonparser.JSONException as error:
-        print ('valid/%d : Failed with error: %s' % (i, error))
+        print('valid/%d: Failed with error: %s' % (i, error))
         continue
 
     py_decoded = json.loads(test)
@@ -25,45 +24,49 @@ for i, test in enumerate(
     try:
         reencoded = jsonparser.decode(py_reencoded)
     except jsonparser.JSONException as error:
-        print ('valid/%d : Failed on re-encoded version with error: %s' % (i, error))
+        print('valid/%d: Failed on re-encoded version with error: %s'
+              % (i, error))
         continue
 
     if reencoded != py_decoded:
-        print ('valid/%d : %s:\n\n%s\n\nbecame\n\n%s\n' % (i, failed, test, reencoded))
+        print('valid/%d: %s:\n\n%s\n\nbecame\n\n%s\n'
+              % (i, failed, test, reencoded))
     else:
-        print ('valid/%d : %s' % (i, passed))
+        print('valid/%d: %s' % (i, passed))
 
-for i, test in enumerate(
-        map(lambda file: open(file).read(), sorted(glob.glob('invalid*.json')))):
+for i, test in enumerate(map(lambda file: open(file).read(),
+                             sorted(glob.glob('invalid*.json')))):
 
     try:
         jsonparser.decode(test)
     except jsonparser.JSONException as error:
-        print ('invalid/%d : %s: %s' % (i, passed, error))
+        print('invalid/%d: %s: %s' % (i, passed, error))
         continue
 
-    print ('invalid/%d : %s (parsing succeeded and shouldn\'t have)' % (i, failed))
+    print('invalid/%d: %s (parsing succeeded and shouldn\'t have)'
+          % (i, failed))
 
 
 # Extension tests
 #
-for i, test in enumerate(
-        map(lambda file: open(file).read(), sorted(glob.glob('ext-valid*.json')))):
+for i, test in enumerate(map(lambda file: open(file).read(),
+                         sorted(glob.glob('ext-valid*.json')))):
 
     try:
         decoded = jsonparser.decode(test)
-        print ('ext-valid/%d : %s: %s' % (i, passed, json.dumps(decoded)))
+        print('ext-valid/%d: %s: %s' % (i, passed, json.dumps(decoded)))
     except jsonparser.JSONException as error:
-        print ('ext-valid/%d : Failed with error: %s' % (i, error))
+        print('ext-valid/%d: Failed with error: %s' % (i, error))
         continue
 
-for i, test in enumerate(
-        map(lambda file: open(file).read(), sorted(glob.glob('ext-invalid*.json')))):
+for i, test in enumerate(map(lambda file: open(file).read(),
+                         sorted(glob.glob('ext-invalid*.json')))):
 
     try:
         jsonparser.decode(test)
     except jsonparser.JSONException as error:
-        print ('ext-invalid/%d : %s: %s' % (i, passed, error))
+        print('ext-invalid/%d: %s: %s' % (i, passed, error))
         continue
 
-    print ('ext-invalid/%d : %s (parsing succeeded and shouldn\'t have)' % (i, failed))
+    print ('ext-invalid/%d: %s (parsing succeeded and shouldn\'t have)'
+           % (i, failed))
